6a5822c0c0f2d605c8164b104a993abdc33be780
imad <imadidaliouali@gmail.com>
2023-05-10 12:30:18 +0100

    Add app icon

--- App.js
+++ App.js
@@ -1,12 +1,26 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { LogBox } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import NetInfo from "@react-native-community/netinfo";
 
 import { HomeScreen, DetailsScreen } from './src/screens';
 
 const Stack = createNativeStackNavigator();
 
 function App() {
+  const [isConnected, setIsConnected] = useState(false);
+
+  useEffect(() => {
+    LogBox.ignoreAllLogs();
+    const unsubscribe = NetInfo.addEventListener(state => {
+      console.log("Connection type", state.type);
+      console.log("Is connected?", state.isConnected);
+      setIsConnected(state.isConnected);
+    });
+    unsubscribe();
+  }, []);
+
   return (
     <NavigationContainer>
       <Stack.Navigator screenOptions={{ headerShown: false }}>
