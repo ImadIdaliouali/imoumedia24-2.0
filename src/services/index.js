ad20b51e133ad3bbfd77f760be5dfce9169b710b
imad <imadidaliouali@gmail.com>
2023-05-11 14:36:33 +0100

    remove getPostById function

--- src/services/index.js
+++ src/services/index.js
@@ -10,10 +10,8 @@ const getPosts = (currentPage = 1, category) => IMOUMEDIA24_HTTP_REQUEST.get(END
 
 const getCategories = () => IMOUMEDIA24_HTTP_REQUEST.get(ENDPOINTS.CATEGORIES);
 
-const getPostById = (postId) => IMOUMEDIA24_HTTP_REQUEST.get(`${ENDPOINTS.POSTS}/${postId}`);
-
 const getAuthor = (authorId) => IMOUMEDIA24_HTTP_REQUEST.get(`${ENDPOINTS.USERS}/${authorId}`);
 
 const getImage = (path) => `${IMOUMEDIA24_URL}/${path}`;
 
-export { getPosts, getCategories, getPostById, getAuthor, getImage };
+export { getPosts, getCategories, getAuthor, getImage };
